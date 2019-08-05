850d65109f147f40f6d5fda3711d44d0786e3e35
unknown <isabela_m_silva@saojose.sc.senai.br>
2019-08-05 12:17:19 -0300

    Aula 1 completa

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Pessoa pessoa1 = new Pessoa("isa", 20, 169, 52);
+		pessoa1.setIdade(21);
+		System.out.println("A pessoa " +  pessoa1.getNome() +  " foi cadastrada com sucesso.");
+
+	}
+
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,56 @@
+
+public class Pessoa {
+	private String nome;
+	private Integer idade;
+	private Integer altura;
+	private Integer peso;
+	
+	public Pessoa(String nome, Integer idade, Integer altura, Integer peso) { // Método construtor com campos
+		super();
+		this.nome = nome;
+		this.idade = idade;
+		this.altura = altura;
+		this.peso = peso;
+	}
+
+	public Pessoa() {// Método construtor vazio
+		super();
+		
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	public Integer getAltura() {
+		return altura;
+	}
+
+	public void setAltura(Integer altura) {
+		this.altura = altura;
+	}
+
+	public Integer getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Integer peso) {
+		this.peso = peso;
+	}
+	
+	
+
+}
